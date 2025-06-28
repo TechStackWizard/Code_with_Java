ccb04744ea063e05963aa065e8ef28009ab6c9d8
TechStackWizard <anshullko474@gmail.com>
2025-06-28 22:56:20 +0530

    Add sorting algorithm

---
+++ Sorting.java
@@ -0,0 +1,61 @@
+public class Sorting {
+
+    public static void bubbleSort(int arr[]){
+        int n = arr.length;
+
+        for(int i=0; i<=n-2; i++){
+            for(int j=0; j<=n-2-i; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for(int i=0; i<n; i++){
+            System.out.println(arr[i]+" ");
+        }
+    }
+
+    public static void selectionSort(int arr[]){
+        int n = arr.length;
+        for(int i=0; i<n-1; i++){
+            int minPos = i;
+            for(int j=i+1; j<n; j++){
+                if(arr[minPos] > arr[j]){
+                    minPos = j;
+                }
+            }
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    public static void insertionSort(int arr[]){
+        int n = arr.length;
+        for(int i=1; i<n; i++){
+            int curr = arr[i];
+            int prev = i-1;
+            while(prev >= 0 && curr < arr[prev]){
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            arr[prev+1] = curr;
+        }
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int arr[] = {4,2,1,5,3}; 
+        // bubbleSort(arr);       
+        // selectionSort(arr);       
+        insertionSort(arr);       
+    }
+}
