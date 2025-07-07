3af33f23949875d551d844c392cb8306c4cae466
TechStackWizard <anshullko474@gmail.com>
2025-07-07 22:51:04 +0530

    Add a program for checking Anagarm strings

---
+++ Strings/Anagram.java
@@ -0,0 +1,38 @@
+import java.io.CharArrayReader;
+import java.util.Arrays;
+
+public class Anagram{
+    public static void checkAnagram(String str1, String str2){
+
+        String Nstr1 = new String(str1);
+        String Nstr2 = new String(str2);
+
+        if(str1.length() == str2.length()){
+            char[] charArray1 = Nstr1.toCharArray();
+            char[] charArray2 = Nstr2.toCharArray();
+
+            Arrays.sort(charArray1);
+            Arrays.sort(charArray2);
+
+            boolean isAnaram = Arrays.equals(charArray1, charArray2);
+
+            
+
+            if(isAnaram){
+                System.out.println(str1 + " and " + str2 + " are anagram string");
+            }
+            else{
+                System.out.println(str1 + " and " + str2 + " are not anagram string");
+            }
+        }
+        else{
+            System.out.println("Not possible bcz length are not equal");
+        }
+    }
+
+
+    public static void main(String []args){
+        
+        checkAnagram("race", "care");
+    }
+}
--- Strings/StringCompression.java
+++ Strings/StringCompression.java
@@ -2,6 +2,7 @@ public class StringCompression {
 
     public static void printCompressedStr(String str) {
         StringBuilder sb = new StringBuilder("");
+        
 
         for (int i = 0; i < str.length(); i++) {
             Integer count = 0;
@@ -17,13 +18,14 @@ public class StringCompression {
                 sb.append(count.toString());
                 i--;
             }
+
             else{
                 sb.append(ch);
             }
-          
-        }
-        System.out.println(sb.toString());
 
+        }
+
+        System.out.println(sb.toString());
     }
 
     public static void main(String[] args) {
