b9c4d2a74876ef823fc9365ac8f08a923ac95edb
TechStackWizard <anshullko474@gmail.com>
2025-10-03 20:38:47 +0530

    Greater Next Element at first and is right to that element

--- Stack_Prac.java/Practice.java
+++ Stack_Prac.java/Practice.java
@@ -46,6 +46,28 @@ public class Practice {
         pushAtBottom(s, data);
     }
 
+    // Next Greater Element in an array is as first greater that is right of that
+    // elemen
+    public static void greaterAtRight(int arr[]) { // Time Complexity : O(n^2)
+        for (int i = 0; i < arr.length; i++) {
+            boolean isFound = false;
+            int j = i + 1;
+            while (!isFound && j < arr.length) {
+                if (arr[i] < arr[j]) {
+                    isFound = true;
+                    arr[i] = arr[j];
+                }
+                j++;
+            }
+            if (!isFound)
+                arr[i] = -1;
+        }
+
+        for (int ele : arr) {
+            System.out.print(ele + " ");
+        }
+    }
+
     public static void main(String[] args) {
         Stack<Integer> s = new Stack<>();
         s.push(1);
@@ -58,7 +80,10 @@ public class Practice {
         // System.out.println(revereString("loop"));
 
         // 1->2->3
-        reverseStack(s);
-        printStack(s); // 3->2->1
+        // reverseStack(s);
+        // printStack(s); // 3->2->1
+
+        int arr[] = { 6, 8, 0, 1, 3 };
+        greaterAtRight(arr);
     }
 }
